52340bd60cd638329ab484b6dec743325ffe69b1
EvanGriffin21 <98298864+EvanGriffin21@users.noreply.github.com>
2022-11-28 17:06:57 -0600

    params

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var express = require("express")
 var path = require("path")
 var mongoose = require("mongoose");
 var session = require("express-session");
+var cookieParser = require("cookie-parser");
+var passport = require("passport");
+var flash = require("connect-flash");
+var params = require("./params/params");
 const { application } = require("express");
+
 //create instance of app
 var app = express();
-
+mongoose.connect(params.DATABASECONNECTION, {useUnifiedTopology:true, useNewUrlParser:true, useCreateIndex:true});
 //connect top database later if needed
 
 app.set("port", process.env.PORT || 3000)
---
+++ params/params.js
@@ -0,0 +1,5 @@
+//Store api keys
+const DATABASECONNECTION = "mongodb+srv://Evan21:121Goat121@cluster0.0nkzijr.mongodb.net/?retryWrites=true&w=majority";
+module.exports = {
+    DATABASECONNECTION
+}
