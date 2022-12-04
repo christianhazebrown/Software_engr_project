b0b3c870bcb311c50beecbe3d6c6bc5c6bb279fc
christianhazebrown <61254256+christianhazebrown@users.noreply.github.com>
2022-12-03 23:15:39 -0600

    finshed
    only one bug for showing the cart  on cart page

--- routes/web/customer.js
+++ routes/web/customer.js
@@ -2,6 +2,7 @@ var express = require("express");
 var Post = require("../../models/movie");
 var ensureAuthenticated = require("../../auth/auth").ensureAuthenticated
 let cart = []
+var display = []
 let i=0
 var router = express.Router();
 // ensure the user is logged in
@@ -14,6 +15,21 @@ router.get("/" , function(req,res){
         res.render("customer/catalog", {post: post});
     });
 });
+
+router.get("/cart" , function(req,res){
+    for(i=0;i <=cart.length-1;i++){
+        Post.findById(cart[i]).exec(function(err, post){
+            //console.log(post)
+            if(err){console.log(err)}
+            display[i] = post
+            console.log("display:", display[i])
+        });
+        // console.log("display:", display[0])
+    res.render("customer/cart", {display: display});
+    }
+
+
+});
 //request the id of the movie from database
 router.get("/:post_id", function(req,res){
     Post.findById(req.params.post_id).exec(function(err, post){
@@ -25,6 +41,6 @@ router.get("/cart/:post_id", function(req,res){
     cart[i] = req.params.post_id
     res.redirect("/catalog")
     i++
-    console.log(cart)
+    //console.log(cart)
 })
 module.exports = router;
